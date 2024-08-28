ff912f3aa71d332d727d0396f2f7dec26a4202e7
Beniamino-Soccio <beniamino43@gmail.com>
2024-08-28 15:53:46 +0200

    created teammates array

--- js/script.js
+++ js/script.js
@@ -32,3 +32,14 @@ Angela Lopez  |	Social Media Manager |	angela-lopez-social-media-manager.jpg
 Scott Estrada |	Developer            |	scott-estrada-developer.jpg
 Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg
 */
+
+//creazione array di oggetti con i dati
+const teammates = [
+    {name: 'Wayne Barnett', role: 'Founder & CEO', photo: 'wayne-barnett-founder-ceo.jpg'},
+    {name: 'Angela Caroll', role: 'Chief Editor', photo: 'angela-caroll-chief-editor.jpg'},
+    {name: 'Walter Gordon', role: 'Office Manager', photo: 'walter-gordon-office-manager.jpg'},
+    {name: 'Angela Lopez', role: 'Social Media Manager', photo: 'angela-lopez-social-media-manager.jpg'},
+    {name: 'Scott Estrada', role: 'Developer', photo: 'scott-estrada-developer.jpg'},
+    {name: 'Barbara Ramos', role: 'Graphic Designer', photo: 'barbara-ramos-graphic-designer.jpg'},
+]
+console.table(teammates);
